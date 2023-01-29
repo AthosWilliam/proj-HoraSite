d6e1855bc4d7bbf2204ffa51f46ccd8784a14153
Athos William <120532271+AthosWilliam@users.noreply.github.com>
2023-01-29 15:38:38 -0300

    Update logic.js

--- src/js/logic.js
+++ src/js/logic.js
@@ -6,14 +6,17 @@ function carregar(){
     var min = data.getMinutes()
     relogio = hora + ':' + min
 
-    //var hora = 19
-
     if (hora >= 0 && hora < 12){
         msg.innerHTML = `Agora são ${relogio} horas da manhã.`
         img.src = "./src/img/manha.png"
+        document.body.style.background = "#3ED5F8"
+
+    } else if (hora >= 12 && hora < 17){
+        msg.innerHTML = `Agora são ${relogio} horas da tarde.`
+        img.src = "./src/img/manha.png"
         document.body.style.background = "#169BEF"
 
-    } else if (hora >= 12 && hora < 18){
+    } else if (hora >= 17 && hora < 19){
         msg.innerHTML = `Agora são ${relogio} horas da tarde.`
         img.src = "./src/img/tarde.png"
         document.body.style.background = "#D98642"
